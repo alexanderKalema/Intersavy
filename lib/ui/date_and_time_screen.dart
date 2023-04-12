ea8ce2846cd394278f59be9f0827cd19f9ca7609
Alexander K. Alema <alexanderkalema@gmail.com>
2023-04-12 18:09:47 +0300

    Task-1 Added routes to plain screens

---
+++ lib/ui/date_and_time_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DateAndTimeScreen extends StatelessWidget {
+  const DateAndTimeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        body: const Text('Date and time Screen')
+    );
+  }
+}
